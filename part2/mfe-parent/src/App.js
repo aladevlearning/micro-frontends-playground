9b876751c546cd87519b0cf57d105789b43510a3
ala <ala@zmags.com>
2021-03-29 23:31:27 +0200

    use hook for auth state

--- part2/mfe-parent/src/App.js
+++ part2/mfe-parent/src/App.js
@@ -1,21 +1,39 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { AmplifySignOut, AmplifyAuthenticator, AmplifySignIn } from '@aws-amplify/ui-react'
+import { onAuthUIStateChange } from '@aws-amplify/ui-components'
+
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
 Amplify.configure(awsconfig);
 
 function App() {
+  const [authState, setAuthState] = React.useState();
+
+  React.useEffect(() => {
+    return onAuthUIStateChange(newAuthState => {
+      setAuthState(newAuthState)
+    });
+  }, []);
+
   return (
     <div className="App">
       <AmplifyAuthenticator>
-        <AmplifySignIn hideSignUp />
-        <AmplifySignOut />
-        <app-accounts></app-accounts>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <app-payments></app-payments>
+        {authState !== "signedin" &&
+          <AmplifySignIn hideSignUp />
+        }
+        {authState === "signedin" &&
+          <div>
+            <AmplifySignOut />
+            <app-accounts></app-accounts>
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+            </header>
+            <app-payments></app-payments>
+          </div>
+        }
+
       </AmplifyAuthenticator>
     </div>
   );
